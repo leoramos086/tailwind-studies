85c7dbfc5697ed79c915a461e8771f0197891a93
leandro-oliveira <leandro.oliveira@unyleya.edu.br>
2024-05-14 01:36:01 -0300

    lang: pt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"]
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'pt-br'
+      }
+    }
+  }
 })
